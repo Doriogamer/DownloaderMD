257b3f2a9642b5977a983099801f480848209241
Doriogamer <derickhumbertoa@gmail.com>
2026-07-12 09:50:29 -0400

    Refactor setup.py for package configuration

--- setup.py
+++ setup.py
@@ -1,45 +1,26 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 with open("requirements.txt", "r", encoding="utf-8") as fh:
-    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
+    requirements = fh.read().splitlines()
 
 setup(
-    name="DownloaderMD",
+    name="downloadMD",
     version="1.0.0",
     author="Doriogamer",
-    author_email="tu_email@ejemplo.com",
-    description="Descargador universal para YouTube, MP3 y links directos con interfaz gráfica",
+    author_email="dalvarezwallace2@gmail.com",
+    description="Descargador universal para YouTube, MP3 y links directos",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Doriogamer/DownloaderMD",
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX :: Linux",
-        "Operating System :: MacOS",
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: End Users/Desktop",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Multimedia :: Video",
-        "Topic :: Multimedia :: Sound/Audio",
-    ],
+    py_modules=["downloader"],
     python_requires=">=3.8",
     install_requires=requirements,
     entry_points={
         "console_scripts": [
-            "downloadermd=main:main",
+            "downloadermd=downloader:__main__",
         ],
     },
 )
